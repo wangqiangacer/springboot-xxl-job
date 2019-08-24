7f1299023848fc4042b5f108b6cff8c990e58ade
wangqiang <850994281@qq.com>
2019-08-24 15:10:35 +0800

    springboot整合xxl-job

---
+++ springboot-xxl-job-demo/src/main/java/com/jacken/xxl/job/AppJobApplication.java
@@ -0,0 +1,11 @@
+package com.jacken.xxl.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppJobApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppJobApplication.class);
+    }
+}
